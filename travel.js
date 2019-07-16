405fe7e8d96d740a8bf2a4bbf9ab0619aeac951c
chaya448 <chaya.fogel1@gmail.com>
2019-07-16 09:55:18 +0300

    travel

---
+++ travel.js
@@ -0,0 +1,52 @@
+
+
+let db = {
+    travelList:[],
+    regList:[]
+}
+
+let Travel = function Travel(name, price){
+    this.Id = db.travelList.length == 0 ? 3 :
+        db.travelList[ db.travelList.length -1 ].Id + 1;
+
+    this.Price = price
+    this.Name = name
+
+    this.getTotalPrice = function getTotalPrice(numOfReg){
+        return numOfReg * this.Price
+    }
+}
+
+Travel.getTravelsCount = function getTravelsCount(){
+    return db.travelList.length;
+}
+Travel.buildTravelHtml = function buildTravelHtml(travelObject){
+    return `<div class="travel-item" travel-id="${travelObject.Id}">
+        <div class="travel-name">${travelObject.Name}</div>
+        <div class="travel-price">${travelObject.Price}</div>
+    </div>`;
+}
+
+//var t1 = new Travel('hiland blue', 100)
+//var t2 = new Travel('hiland green', 100)
+
+
+db.travelList.push(new Travel('hiland', 100))
+db.travelList.push(new Travel('london', 300))
+db.travelList.push(new Travel('miron', 200))
+db.travelList.push(new Travel('Israel', 0))
+db.travelList.push(new Travel('venice', 120))
+db.travelList.push(new Travel('south america', 500))
+db.travelList.push(new Travel('prague', 150))
+db.travelList.push(new Travel('honolulu', 200))
+
+
+let travelListElm = document.getElementById('travel-list')
+for (let i = 0; i < db.travelList.length; i++) {
+    let _t = db.travelList[i];
+    let h = Travel.buildTravelHtml(_t)
+    travelListElm.innerHTML += h;
+}
+
+
+
